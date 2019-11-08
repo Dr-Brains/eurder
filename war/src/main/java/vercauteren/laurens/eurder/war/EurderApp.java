9225d3826568f45ed5e6750adbe23aed5e133252
Laurens Vercauteren <laurens.vercauteren@gmail.com>
2019-11-08 11:29:54 +0100

    Initial project setup

---
+++ war/src/main/java/vercauteren/laurens/eurder/war/EurderApp.java
@@ -0,0 +1,12 @@
+package vercauteren.laurens.eurder.war;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+	@SpringBootApplication(scanBasePackages = "vercauteren.laurens.eurder")
+public class EurderApp {
+
+		public static void main(String[] args) {
+			SpringApplication.run(EurderApp.class, args);
+		}
+	}
